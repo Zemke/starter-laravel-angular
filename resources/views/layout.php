543bd20c8251a927331b2fd2fafb58f66a9e37fd
Zemke <florian.zemke@ewetel.net>
2015-02-24 11:46:34 +0100

    Added Bootstrap template.

--- resources/views/layout.php
+++ resources/views/layout.php
@@ -10,13 +10,32 @@
     <script type="application/javascript" src="/js/appRoutes.js"></script>
     <script type="application/javascript" src="/js/controllers/MainController.js"></script>
     <script type="application/javascript" src="/js/app.js"></script>
+    <link rel="stylesheet" href="/css/app.css"/>
 </head>
 <body ng-app="todoApp" ng-controller="MainController">
 
-<div class="container">
-    <a href="#articles">articles</a>
-    <div ng-view>
+<nav class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#/">Todos</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="#/">Home</a></li>
+                <li><a href="#/articles">Articles</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
 
+<div class="container" style="padding-top: 80px;">
+    <div ng-view>
     </div>
 </div>
 
--- resources/views/partials/index.php
+++ resources/views/partials/index.php
@@ -1,13 +1,1 @@
-<div class="container">
-    <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">Home</div>
-
-                <div class="panel-body">
-                    This is the index.
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+This is the index.
