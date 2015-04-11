a9b9dfe5d4109358eb813e175f7e02ae2d5a9c18
Zemke <florian.zemke@ewetel.net>
2015-04-12 00:10:25 +0200

    Sexier forms.

--- resources/views/partials/auth/signup.php
+++ resources/views/partials/auth/signup.php
@@ -1,16 +1,32 @@
-<form name="loginForm" ng-controller="UserController"
-      ng-submit="create()" novalidate>
-    <label for="username">Username:</label>
-    <input type="text" id="username"
-           ng-model="username">
-    <label for="username">Email:</label>
-    <input type="text" id="email"
-           ng-model="email">
-    <label for="password">Password:</label>
-    <input type="password" id="password"
-           ng-model="password">
-    <label for="passwordConfirmation">Password Confirmation:</label>
-    <input type="password" id="passwordConfirmation"
-           ng-model="passwordConfirmation">
-    <button type="submit">Login</button>
+<form name="loginForm" ng-controller="UserController" ng-submit="create()"
+      class="form-horizontal" novalidate>
+    <div class="form-group">
+        <div class="col-md-3">
+            <input type="text" id="username" ng-model="username"
+                   class="form-control" placeholder="Username">
+        </div>
+    </div>
+    <div class="form-group">
+        <div class="col-md-3">
+            <input type="text" id="email" ng-model="email"
+                   class="form-control" placeholder="Email">
+        </div>
+    </div>
+    <div class="form-group">
+        <div class="col-md-3">
+            <input type="password" id="password" ng-model="password"
+                   class="form-control" placeholder="Password">
+        </div>
+    </div>
+    <div class="form-group">
+        <div class="col-md-3">
+            <input type="password" id="passwordConfirmation" ng-model="passwordConfirmation"
+                   class="form-control" placeholder="Password again">
+        </div>
+    </div>
+    <div class="form-group">
+        <div class="col-md-4">
+            <button type="submit" class="btn btn-primary">Sign Up</button>
+        </div>
+    </div>
 </form>
