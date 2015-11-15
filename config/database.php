dbb294c28792723b2065113e9c92762c8839cef9
Zemke <Florian.Zemke@ewetel.net>
2015-11-15 17:17:15 +0100

    Rolled back DB settings to default

--- config/database.php
+++ config/database.php
@@ -54,9 +54,9 @@ return [
 
 		'mysql' => [
 			'driver'    => 'mysql',
-			'host'      => env('DB_HOST', '127.0.0.1'),
-			'database'  => env('DB_DATABASE', 'tel'),
-			'username'  => env('DB_USERNAME', 'root'),
+			'host'      => env('DB_HOST', 'localhost'),
+			'database'  => env('DB_DATABASE', 'forge'),
+			'username'  => env('DB_USERNAME', 'forge'),
 			'password'  => env('DB_PASSWORD', ''),
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
