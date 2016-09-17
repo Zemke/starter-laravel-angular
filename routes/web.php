f217fc793f3d80ba2ea467e41e350f6592f7be88
Zemke <flzemke@gmail.com>
2016-09-17 19:33:35 +0200

    Setup basic routes and init page

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,11 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layout');
 });
+
+
+// Catch all undefined routes. Always gotta stay at the bottom since order of routes matters.
+Route::any('{undefinedRoute}', function ($undefinedRoute) {
+    return view('layout');
+})->where('undefinedRoute', '([A-z\d-\/_.]+)?');
