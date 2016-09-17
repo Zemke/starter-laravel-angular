16859e0d2fd0b24f3bd1808fdba549b98d63854f
Zemke <flzemke@gmail.com>
2016-09-17 20:10:13 +0200

    Add partial routes

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,17 @@ Route::get('/', function () {
     return view('layout');
 });
 
+Route::get('/partials/index', function () {
+    return view('partials.index');
+});
+
+Route::get('/partials/{category}/{action?}', function ($category, $action = 'index') {
+    return view(join('.', ['partials', $category, $action]));
+});
+
+Route::get('/partials/{category}/{action}/{id}', function ($category, $action = 'index', $id) {
+    return view(join('.', ['partials', $category, $action]));
+});
 
 // Catch all undefined routes. Always gotta stay at the bottom since order of routes matters.
 Route::any('{undefinedRoute}', function ($undefinedRoute) {
